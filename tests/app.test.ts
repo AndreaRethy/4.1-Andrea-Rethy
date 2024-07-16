00274074884095b8d5c7d1545f70760e53d033b1
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-07-16 13:11:59 +0200

    Add test for addNewTask

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -6,9 +6,16 @@ describe('New To Do List', () => {
   const task = new Task({ "task" : "test task" });
   const toDoList = new ToDoList([task]);
 
-    test('should have addNewTask defined', () => {
+    test('should have methods defined', () => {
       expect(toDoList.addNewTask).toBeDefined();
       expect(toDoList.deleteTask).toBeDefined();
       expect(toDoList.updateTask).toBeDefined();
     });
+
+    test('should call addNewTask with task2', () => {
+      const task2 = new Task({ "task" : "test task 2" });
+      toDoList.addNewTask(task2)
+
+      expect(toDoList.toDoList[1].task).toBe("test task 2");
+    })
   });
