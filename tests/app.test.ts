a0ec1550d7466245f484279bd60e9adf7f790639
Andrea Rethy <125359387+AndreaRethy@users.noreply.github.com>
2024-07-16 12:58:27 +0200

    Add first tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,6 +1,14 @@
+import Task from '../src/domain/entities/Task';
+import ToDoList from '../src/domain/entities/ToDoList';
+
 // Green test
-describe('Sample Test', () => {
-    it('should return true', () => {
-      expect(true).toBe(true);
+describe('New To Do List', () => {
+  const task = new Task({ "task" : "test task" });
+  const toDoList = new ToDoList([task]);
+
+    test('should have addNewTask defined', () => {
+      expect(toDoList.addNewTask).toBeDefined();
+      expect(toDoList.deleteTask).toBeDefined();
+      expect(toDoList.updateTask).toBeDefined();
     });
   });
